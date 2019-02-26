01a2fb731bd69c89d96e45ea26c22ed12bfd9aca
irinaromanova <46283789+irinaromanova@users.noreply.github.com>
2019-02-26 21:40:31 +0300

    Форма обратной связи

--- js/wrightus.js
+++ js/wrightus.js
@@ -1,15 +1,15 @@
 var link = document.querySelector(".button-wrightus");
 var popup = document.querySelector(".wright-us-modal");
 var close = popup.querySelector(".modal-close");
-var name = popup.querySelector(".wright-name");
-var form = popup.querySelector(".wright-us-modal");
-var email = popup.querySelector(".wrightemail");
+var yourName = popup.querySelector(".wright-name");
+
+var email = popup.querySelector(".wright-email");
 var letterText = popup.querySelector(".wright-letter");
 var isStorageSupport = true;
 var storage = "";
 
 try {
- storage = localStorage.getItem("name");
+ storage = localStorage.getItem("yourName");
  storage = localStorage.getItem("email");
 } catch (err) {
  isStorageSupport = false;
@@ -19,11 +19,11 @@ link.addEventListener("click", function (evt) {
  evt.preventDefault();
  popup.classList.add("modal-show");
  if (storage) {
-  name.value = storage;
+  yourName.value = storage;
   email.value = storage;
   letterText.focus();
  } else {
-  name.focus();
+  yourName.focus();
  }
 });
 
@@ -33,15 +33,15 @@ close.addEventListener("click", function (evt) {
  popup.classList.remove("modal-error");
 });
 
-form.addEventListener("submit", function (evt) {
-  if (!name.value || !email.value) {
+popup.addEventListener("submit", function (evt) {
+  if (!yourName.value || !email.value) {
    evt.preventDefault();
    popup.classList.remove("modal-error");
    popup.offsetWidth = popup.offsetWidth;
    popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
-     localStorage.setItem("name", name.value);
+     localStorage.setItem("yourName", yourName.value);
      localStorage.setItem("email", email.value);
     }
  }
